2e6a3be9251b83be837cc4f1b9ec3e20b5ce6e29
Esmeralda Ayala Mendoza <Jiesm00@hotmail.com>
2026-03-09 09:34:50 -0600

    First comments

--- Hubs/GameHub.cs
+++ Hubs/GameHub.cs
@@ -1,3 +1,6 @@
+//Ventanilla de comunicación inicial
+//Aquí se escribirá las acciones de Angular, se pedirá en el backend, a través de SingalR
+
 using Microsoft.AspNetCore.SignalR;
 using GatoBackend.Models;
 using GatoBackend.Services;
